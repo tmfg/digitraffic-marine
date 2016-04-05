049619cea626e630e5ad4825d7965cd7b248197f
Petri Sirkkala <sirpete@iki.fi>
2016-04-05 13:15:08 +0300

    LD-28 Retry support added to app

--- src/main/java/fi/livi/digitraffic/meri/AisApplication.java
+++ src/main/java/fi/livi/digitraffic/meri/AisApplication.java
@@ -2,10 +2,12 @@ package fi.livi.digitraffic.meri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 public class AisApplication {
     public static void main(String[] args) {
         SpringApplication.run(AisApplication.class, args);
