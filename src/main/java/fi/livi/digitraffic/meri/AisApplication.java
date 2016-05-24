e2186a45d7d8e3cc42b4967473380ebd569f17ee
Teijo Rönkä <teijo.ronka@solita.fi>
2016-05-24 10:01:45 +0300

    added CoverageIgnore

--- src/main/java/fi/livi/digitraffic/meri/AisApplication.java
+++ src/main/java/fi/livi/digitraffic/meri/AisApplication.java
@@ -2,13 +2,20 @@ package fi.livi.digitraffic.meri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import fi.livi.digitraffic.meri.annotation.CoverageIgnore;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableRetry
+@EnableTransactionManagement
+@EntityScan(basePackages = {"fi.livi.util", "fi.livi.digitraffic.meri"})
+@ComponentScan(basePackages = {"fi.livi.util", "fi.livi.digitraffic.meri"})
 public class AisApplication {
+    @CoverageIgnore
     public static void main(String[] args) {
         SpringApplication.run(AisApplication.class, args);
     }
