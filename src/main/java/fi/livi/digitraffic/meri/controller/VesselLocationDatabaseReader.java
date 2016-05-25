af0dfe013db8af95ff23efab8297e257c052f080
Teijo Rönkä <teijo.ronka@solita.fi>
2016-05-25 13:22:26 +0300

    changed ais.server.address -> ais.locations.url

--- src/main/java/fi/livi/digitraffic/meri/controller/VesselLocationDatabaseReader.java
+++ src/main/java/fi/livi/digitraffic/meri/controller/VesselLocationDatabaseReader.java
@@ -1,18 +1,19 @@
 package fi.livi.digitraffic.meri.controller;
 
-import fi.livi.digitraffic.meri.dao.VesselLocationRepository;
-import fi.livi.digitraffic.meri.domain.VesselLocation;
-import fi.livi.digitraffic.meri.model.AISMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import fi.livi.digitraffic.meri.dao.VesselLocationRepository;
+import fi.livi.digitraffic.meri.domain.VesselLocation;
+import fi.livi.digitraffic.meri.model.AISMessage;
 
 @Component
 public class VesselLocationDatabaseReader extends VesselLocationReader {
     private VesselLocationRepository vesselLocationRepository;
 
     @Autowired
-    public VesselLocationDatabaseReader(@Value("${ais.server.address}") final String serverAddress,
+    public VesselLocationDatabaseReader(@Value("${ais.locations.url}") final String serverAddress,
                                         final VesselLocationRepository vesselLocationRepository) {
         super(serverAddress);
         this.vesselLocationRepository = vesselLocationRepository;
