9f3169cda490b858c355157c77ffb067fea3084e
Teijo Rönkä <teijo.ronka@solita.fi>
2016-09-05 09:09:33 +0300

    otettu ucp käyttöön

--- src/main/java/fi/livi/digitraffic/meri/config/AisApplicationConfiguration.java
+++ src/main/java/fi/livi/digitraffic/meri/config/AisApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package fi.livi.digitraffic.meri.config;
 
 import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
-import oracle.jdbc.pool.OracleDataSource;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,6 +22,7 @@ public class AisApplicationConfiguration {
     public static final String API_WEBSOCKETS_PART_PATH = "/websockets";
     public static final String API_PLAIN_WEBSOCKETS_PART_PATH = "/plain-websockets";
     public static final String API_METADATA_PART_PATH = "/metadata";
+    public static final String API_LOCATIONS_PATH = "/locations";
 
     /**
      * Initialize OracleDataSource manually because datasource property spring.datasource.type=oracle.jdbc.pool.OracleDataSource
@@ -30,12 +33,18 @@ public class AisApplicationConfiguration {
      */
     @Bean
     public DataSource dataSource(final DataSourceProperties properties) throws SQLException {
-        OracleDataSource dataSource = new OracleDataSource();
+        final PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
         dataSource.setUser(properties.getUsername());
         dataSource.setPassword(properties.getPassword());
         dataSource.setURL(properties.getUrl());
-        dataSource.setImplicitCachingEnabled(true);
         dataSource.setFastConnectionFailoverEnabled(true);
+        dataSource.setMaxPoolSize(20);
+        dataSource.setMinPoolSize(5);
+        dataSource.setMaxIdleTime(5);
+        dataSource.setValidateConnectionOnBorrow(true);
+        dataSource.setMaxStatements(10);
+        dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+
         return dataSource;
     }
 }
