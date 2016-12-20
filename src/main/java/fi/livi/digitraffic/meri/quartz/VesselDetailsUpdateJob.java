c76c1c77328ae1ad527769b272100f1b4aaad198
Ville Kähkönen <ville.kahkonen@solita.fi>
2016-12-20 09:14:21 +0200

    DPO-46 use lang3 stop watch instead of one in spring

--- src/main/java/fi/livi/digitraffic/meri/quartz/VesselDetailsUpdateJob.java
+++ src/main/java/fi/livi/digitraffic/meri/quartz/VesselDetailsUpdateJob.java
@@ -1,11 +1,11 @@
 package fi.livi.digitraffic.meri.quartz;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StopWatch;
 
 import fi.livi.digitraffic.meri.service.portnet.vesseldetails.VesselDetailsUpdater;
 
@@ -21,13 +21,12 @@ public class VesselDetailsUpdateJob extends AbstractUpdateJob {
     public void execute(final JobExecutionContext jobExecutionContext) {
         log.info("Quartz VesselDetailsUpdateJob starting");
 
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+        StopWatch stopWatch = StopWatch.createStarted();
 
         vesselDetailsUpdater.update();
 
         stopWatch.stop();
 
-        log.info("Quartz VesselDetailsUpdateJob ended (took " + stopWatch.getTotalTimeMillis() + " milliseconds)");
+        log.info("Quartz VesselDetailsUpdateJob ended (took " + stopWatch.getTime() + " milliseconds)");
     }
 }
