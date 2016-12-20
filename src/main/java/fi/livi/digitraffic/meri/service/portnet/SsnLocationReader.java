40b17fc6d4ebab6a2b053200bb46dbeda2ecb98f
Teijo Rönkä <teijo.ronka@solita.fi>
2016-12-20 09:20:51 +0200

    LD-15 rename & sonar fixes

--- src/main/java/fi/livi/digitraffic/meri/service/portnet/SsnLocationReader.java
+++ src/main/java/fi/livi/digitraffic/meri/service/portnet/SsnLocationReader.java
@@ -5,12 +5,11 @@ import org.apache.commons.lang3.StringUtils;
 import fi.livi.digitraffic.meri.domain.portnet.SsnLocation;
 
 public class SsnLocationReader extends AbstractReader<SsnLocation> {
-
     @Override
     protected SsnLocation convert(final String line) {
         final String[] components = StringUtils.splitPreserveAllTokens(line, DELIMETER);
 
-        SsnLocation ssnLocation = new SsnLocation();
+        final SsnLocation ssnLocation = new SsnLocation();
         ssnLocation.setLocode(components[0]);
         ssnLocation.setLocationName(getString(components[1]));
         ssnLocation.setCountry(getString(components[2]));
@@ -20,7 +19,7 @@ public class SsnLocationReader extends AbstractReader<SsnLocation> {
         return ssnLocation;
     }
 
-    private String getString(final String s) {
+    private static String getString(final String s) {
         return StringUtils.isEmpty(s) ? null : s;
     }
 }
