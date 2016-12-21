c143658cdca5ecc02165626bb1378d77fd4c994d
Ville Kähkönen <ville.kahkonen@solita.fi>
2016-12-21 15:19:11 +0200

    DPO-46 fix compile errors

--- src/main/java/fi/livi/digitraffic/meri/dao/portnet/VesselDetailsRepository.java
+++ src/main/java/fi/livi/digitraffic/meri/dao/portnet/VesselDetailsRepository.java
@@ -9,11 +9,10 @@ import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import fi.livi.digitraffic.meri.domain.portnet.VesselDetails.VesselDetails;
-import fi.livi.digitraffic.meri.model.portnet.metadata.VesselDetailsJson;
 
 @Repository
 public interface VesselDetailsRepository extends JpaRepository<VesselDetails, Long> {
 
     @QueryHints(@QueryHint(name = "org.hibernate.fetchSize", value = "1000"))
-    List<VesselDetailsJson> findByVesselIdInOrderByVesselIdAsc(final List<Long> vesselIds);
+    List<VesselDetails> findByVesselIdInOrderByVesselIdAsc(final List<Long> vesselIds);
 }
