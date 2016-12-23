7ca54cb7b7806172ede6f11e15df53476f66f563
Teijo Rönkä <teijo.ronka@solita.fi>
2016-12-23 08:07:34 +0200

    LD-15 return Json-objects, not entity-objects use sublists of 1000(because Oracle)

--- src/main/java/fi/livi/digitraffic/meri/service/ais/VesselLocationService.java
+++ src/main/java/fi/livi/digitraffic/meri/service/ais/VesselLocationService.java
@@ -6,22 +6,28 @@ import javax.persistence.EntityManager;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import fi.livi.digitraffic.meri.dao.ais.VesselLocationRepository;
 import fi.livi.digitraffic.meri.domain.ais.VesselLocation;
 import fi.livi.digitraffic.meri.model.ais.VesselLocationJson;
+import fi.livi.digitraffic.util.SublistFetcher;
 
 @Service
 @Transactional(readOnly = true)
 public class VesselLocationService {
     private final EntityManager entityManager;
+    private final VesselLocationRepository vesselLocationRepository;
 
     @Autowired
-    public VesselLocationService(final EntityManager entityManager) {
+    public VesselLocationService(final EntityManager entityManager,
+                                 final VesselLocationRepository vesselLocationRepository) {
         this.entityManager = entityManager;
+        this.vesselLocationRepository = vesselLocationRepository;
     }
 
     private Criteria createCriteria() {
@@ -35,7 +41,7 @@ public class VesselLocationService {
     }
 
     private List<VesselLocationJson> findLocations(final Integer mmsi, final Long from, final Long to) {
-        final Criteria c = createCriteria();
+        final Criteria c = createCriteria().setProjection(Projections.id());
 
         if(mmsi != null) {
             c.add(Restrictions.eq("mmsi", mmsi));
@@ -49,7 +55,7 @@ public class VesselLocationService {
             c.add(Restrictions.le("timestamp", from));
         }
 
-        return c.list();
+        return SublistFetcher.fetch(c.list(), vesselLocationRepository::findByMmsiIn);
     }
 
     public List<VesselLocationJson> findLocations(final Long from, final Long to) {
