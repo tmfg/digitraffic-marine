d0cc97a442262988a95dd77812e678a2b7dd6496
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-03-02 13:11:26 +0200

    DPO-107 include properties in geojson data for consistency

--- src/main/java/fi/livi/digitraffic/meri/model/portnet/metadata/PortAreaProperties.java
+++ src/main/java/fi/livi/digitraffic/meri/model/portnet/metadata/PortAreaProperties.java
@@ -1,10 +1,7 @@
 package fi.livi.digitraffic.meri.model.portnet.metadata;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 import io.swagger.annotations.ApiModelProperty;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PortAreaProperties {
     @ApiModelProperty(value = "Port area name", required = true)
     public final String portAreaName;
