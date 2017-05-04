62c5c7d67bb8a466e6690e5041e4f48106c453ca
Jouni Sonninen <jouni.sonninen@solita.fi>
2017-05-04 14:59:19 +0300

    DPO-143 Lokitetaan pitkäkestoiset service pyynnöt

---
+++ src/main/java/fi/livi/digitraffic/meri/config/AopConfiguration.java
@@ -0,0 +1,25 @@
+package fi.livi.digitraffic.meri.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import fi.livi.digitraffic.meri.aop.PerformanceMonitorAspect;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@Aspect
+public class AopConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(AopConfiguration.class);
+    private PerformanceMonitorAspect performanceMonitorAspect;
+
+    @Bean
+    public PerformanceMonitorAspect performanceMonitorAspect() {
+        this.performanceMonitorAspect = new PerformanceMonitorAspect();
+        return performanceMonitorAspect;
+    }
+}
