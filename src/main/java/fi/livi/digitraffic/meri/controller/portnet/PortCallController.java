9caffa129af7ce92df41ff1aa240aca3308bf9d7
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-08-25 11:48:08 +0300

    DPO-242 default parameter value for port calls api

--- src/main/java/fi/livi/digitraffic/meri/controller/portnet/PortCallController.java
+++ src/main/java/fi/livi/digitraffic/meri/controller/portnet/PortCallController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fi.livi.digitraffic.meri.model.portnet.data.PortCallsJson;
-import fi.livi.digitraffic.meri.service.BadRequestException;
 import fi.livi.digitraffic.meri.service.portnet.PortCallService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -39,7 +38,6 @@ public class PortCallController {
     @ApiOperation("Find port calls")
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of port calls"),
-                    @ApiResponse(code = 400, message = "Bad request. At least one parameter is required."),
                     @ApiResponse(code = 500, message = "Internal server error") })
     @ResponseBody
     public PortCallsJson listAllPortCalls(
@@ -47,9 +45,10 @@ public class PortCallController {
             @RequestParam(value = "date", required = false)
             @DateTimeFormat(iso = DATE) final Date date,
 
-            @ApiParam("Return port calls received after given time in ISO date format {yyyy-MM-dd'T'HH:mm:ss.SSSZ} e.g. 2016-10-31T06:30:00.000Z.")
+            @ApiParam("Return port calls received after given time in ISO date format {yyyy-MM-dd'T'HH:mm:ss.SSSZ} e.g. 2016-10-31T06:30:00.000Z. " +
+                      "Default value is now minus 24 hours if all parameters are empty.")
             @RequestParam(value = "from", required = false)
-            @DateTimeFormat(iso = DATE_TIME) final ZonedDateTime from,
+            @DateTimeFormat(iso = DATE_TIME) ZonedDateTime from,
 
             @ApiParam("Return port calls for given vessel name")
             @RequestParam(value = "vesselName", required = false) final String vesselName,
@@ -68,7 +67,7 @@ public class PortCallController {
             ) {
 
         if(!ObjectUtils.anyNotNull(date, from, vesselName, mmsi, imo, nationality, vesselTypeCode)) {
-            throw new BadRequestException("At least one parameter is required");
+            from = ZonedDateTime.now().minusDays(1);
         }
 
         return portCallService.findPortCalls(date, from, null, vesselName, mmsi, imo, nationality, vesselTypeCode);
@@ -77,7 +76,6 @@ public class PortCallController {
     @ApiOperation("Find port calls")
     @GetMapping(path = "/{locode}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ApiResponses({ @ApiResponse(code = 200, message = "Successful retrieval of port calls"),
-                    @ApiResponse(code = 400, message = "Bad request. At least one parameter is required."),
                     @ApiResponse(code = 500, message = "Internal server error") })
     @ResponseBody
     public PortCallsJson listAllPortCallsFromLocode(
@@ -107,11 +105,6 @@ public class PortCallController {
             @ApiParam("Return port calls for given vessel type code")
             @RequestParam(value = "vesselTypeCode", required = false) final Integer vesselTypeCode
     ) {
-
-        if(!ObjectUtils.anyNotNull(date, from, vesselName, mmsi, imo, nationality, vesselTypeCode)) {
-            throw new BadRequestException("At least one parameter is required");
-        }
-
         return portCallService.findPortCalls(date, from, locode, vesselName, mmsi, imo, nationality, vesselTypeCode);
     }
 }
