tmfg/digitraffic-road

Asetettu Javamelody:lle järkevä nimi lokitukseen.

src/main/java/fi/livi/digitraffic/tie/conf/JavaMelodyConfiguration.java
@@ -53,7 +53,7 @@ public class JavaMelodyConfiguration implements ServletContextInitializer {
         final MonitoringFilter filter = new MonitoringFilter();
         javaMelody.setFilter(filter);
         javaMelody.setAsyncSupported(true);
-        javaMelody.setName("JavaMelody");
+        javaMelody.setName("net.bull.javamelody.MetaMelody");
         javaMelody.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC);
 
         // see the list of parameters:

tmfg/digitraffic-marine

src/main/java/fi/livi/digitraffic/meri/config/JavaMelodyConfiguration.java
@@ -54,7 +54,7 @@ public class JavaMelodyConfiguration implements ServletContextInitializer {
         final MonitoringFilter filter = new MonitoringFilter();
         javaMelody.setFilter(filter);
         javaMelody.setAsyncSupported(true);
-        javaMelody.setName("javamelody");
+        javaMelody.setName("net.bull.javamelody.AISMelody");
         javaMelody.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC);
 
         // see the list of parameters: