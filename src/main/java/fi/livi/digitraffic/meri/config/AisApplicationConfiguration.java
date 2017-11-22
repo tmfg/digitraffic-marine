c8963a0f307d569dc7bf4d8807b087f0d0660345
Petri Sirkkala <sirpete@iki.fi>
2017-11-22 16:11:56 +0200

    DPO-319 Loput datasource parannukset tuotu takaisin aiemmasta - Metadataan initial-, min- ja max-poolsize asetus

--- src/main/java/fi/livi/digitraffic/meri/config/AisApplicationConfiguration.java
+++ src/main/java/fi/livi/digitraffic/meri/config/AisApplicationConfiguration.java
@@ -27,7 +27,7 @@ public class AisApplicationConfiguration {
     public static final String API_LOCATIONS_PATH = "/locations";
     public static final String API_PORT_CALLS_PATH = "/port-calls";
 
-    // Katso POOL_SIZE selitykset käyttökohdassa
+    @SuppressWarnings("NonConstantFieldWithUpperCaseName")
     @Value("${ci.datasource.initialPoolSize:20}")
     private Integer INITIAL_POOL_SIZE;
     @Value("${ci.datasource.maxPoolSize:20}")
@@ -57,13 +57,49 @@ public class AisApplicationConfiguration {
         dataSource.setUser(properties.getUsername());
         dataSource.setPassword(properties.getPassword());
         dataSource.setURL(properties.getUrl());
+        dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+        /*
+         * https://docs.oracle.com/cd/E11882_01/java.112/e16548/fstconfo.htm#JJDBC26000
+         */
         dataSource.setFastConnectionFailoverEnabled(true);
+        /*
+         * https://docs.oracle.com/cd/E18283_01/java.112/e12265/connect.htm#CHDIDJGH
+         */
+        dataSource.setValidateConnectionOnBorrow(true);
+
+        /* ****************************************************************************************************
+         * Settings below based on:
+         * https://docs.oracle.com/cd/B28359_01/java.111/e10788/optimize.htm#CHDEHFHE
+         */
+
+        /*
+         * https://review.solita.fi/cru/CR-4219#c52811
+         * initial max ja min kaikki samaan arvoon. Yhteyden avaus on raskas operaatio, mitä halutaan välttää. Kuudennen rinnakkaisen yhteyden
+         * tarvitsemishetkellää kanta on todennäköisesti kuormitettuna ja haluamme välttää yhteyden avaamisesta aiheutuvaa ylimääräistä kuormaa.
+         *
+         * Mutta koska meidän CI-pannuilla ei ole juhlavaa määrää conneja,
+         * on ci-buildeissa tälle pienempi arvo. Default on tuolla POOL_SIZE muuttujassa.
+         */
         dataSource.setInitialPoolSize(INITIAL_POOL_SIZE);
         dataSource.setMaxPoolSize(MAX_POOL_SIZE);
         dataSource.setMinPoolSize(MIN_POOL_SIZE);
-        dataSource.setValidateConnectionOnBorrow(true);
-        dataSource.setMaxStatements(10);
-        dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+
+        /*
+         * See:
+         * https://docs.oracle.com/cd/B28359_01/java.111/e10788/optimize.htm#CFHEDJDC
+         *
+         * The cache size should be set to the number of distinct statements the application issues to the database.
+         */
+        dataSource.setMaxStatements(25);
+        /* The abandoned connection timeout enables borrowed connections to be reclaimed back into the connection pool after a connection
+         * has not been used for a specific amount of time. Abandonment is determined by monitoring calls to the database. */
+        dataSource.setAbandonedConnectionTimeout(60);
+        /* The time-to-live connection timeout enables borrowed connections to remain borrowed for a specific amount of time before the
+         * connection is reclaimed by the pool. This timeout feature helps maximize connection reuse and helps conserve systems resources
+         * that are otherwise lost on maintaining connections longer than their expected usage. */
+        dataSource.setTimeToLiveConnectionTimeout(480);
+        /* **************************************************************************************************** */
+
         return dataSource;
     }
 }
