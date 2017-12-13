cee0fc848228944ba1bb249c9579100525eb625f
Petri Sirkkala <sirpete@iki.fi>
2017-12-13 10:35:33 +0200

    Added simple instructions for running the test - Fixed bits19to16 bit mask.

--- scripts/test-ais-eta.js
+++ scripts/test-ais-eta.js
@@ -1,3 +1,6 @@
+// Run this script using node-interpreter.
+// See: node.js
+
 const https = require('https');
 
 https.get('https://meri.digitraffic.fi/api/v1/metadata/vessels/249500000', (resp) => {
@@ -15,7 +18,7 @@ https.get('https://meri.digitraffic.fi/api/v1/metadata/vessels/249500000', (resp
     let bits0to5 = eta & 0b111111;
     let bits6to10 = eta >>> 6 & 0b11111;
     let bits11to15 = eta >>> 11 & 0b11111;
-    let bits16to19 = eta >>> 16 & 0b01111;
+    let bits16to19 = eta >>> 16 & 0b11111;
     console.log('eta:', eta);
     console.log('Bits 5-0: minute:', bits0to5);
     console.log('Bits 10-6: hour:', bits6to10);
