634b8a9cfd3828ddfe116ff27737fa538e168d28
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-12-13 10:04:25 +0200

    Testi scriba korjattu

--- scripts/test-ais-eta.js
+++ scripts/test-ais-eta.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-https.get('https://meri.digitraffic.fi/api/v1/metadata/vessels/230991010', (resp) => {
+https.get('https://meri.digitraffic.fi/api/v1/metadata/vessels/249500000', (resp) => {
   let data = '';
 
   // A chunk of data has been recieved.
@@ -12,10 +12,10 @@ https.get('https://meri.digitraffic.fi/api/v1/metadata/vessels/230991010', (resp
   resp.on('end', () => {
     console.log('JSON:', JSON.parse(data));
     let eta = JSON.parse(data).eta;
-    let bits0to5 = eta & 0b11111;
-    let bits6to10 = eta >>> 5 & 0b11111;
-    let bits11to15 = eta >>> 10 & 0b11111;
-    let bits16to19 = eta >>> 15 & 0b01111;
+    let bits0to5 = eta & 0b111111;
+    let bits6to10 = eta >>> 6 & 0b11111;
+    let bits11to15 = eta >>> 11 & 0b11111;
+    let bits16to19 = eta >>> 16 & 0b01111;
     console.log('eta:', eta);
     console.log('Bits 5-0: minute:', bits0to5);
     console.log('Bits 10-6: hour:', bits6to10);
