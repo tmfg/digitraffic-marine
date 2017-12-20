fe613dfad90a521ea8ad959a516f8a5bad3d9bb8
Ville Kähkönen <ville.kahkonen@solita.fi>
2017-12-20 13:14:14 +0200

    XXX vanhat datasource-asetukset takaisin - Testit updatePortCallsSucceeds ja updateVesselDetailsSucceeds rikki, koska updated_time-kenttää muutetaan ZonedDateTimeksi

--- src/main/java/fi/livi/digitraffic/meri/config/AisApplicationConfiguration.java
+++ src/main/java/fi/livi/digitraffic/meri/config/AisApplicationConfiguration.java
@@ -1,10 +1,18 @@
 package fi.livi.digitraffic.meri.config;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -28,4 +36,26 @@ public class AisApplicationConfiguration {
         return new MethodValidationPostProcessor();
     }
 
+    /**
+     * Initialize OracleDataSource manually because datasource property spring.datasource.type=oracle.jdbc.pool.OracleDataSource
+     * is not correctly handled by spring https://github.com/spring-projects/spring-boot/issues/6027#issuecomment-221582708
+     * @param properties
+     * @return
+     * @throws SQLException
+     */
+    @Bean
+    public DataSource dataSource(final DataSourceProperties properties) throws SQLException {
+        final PoolDataSource dataSource = PoolDataSourceFactory.getPoolDataSource();
+        dataSource.setUser(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+        dataSource.setURL(properties.getUrl());
+        dataSource.setFastConnectionFailoverEnabled(true);
+        dataSource.setMaxPoolSize(20);
+        dataSource.setMinPoolSize(5);
+        dataSource.setMaxIdleTime(5);
+        dataSource.setValidateConnectionOnBorrow(true);
+        dataSource.setMaxStatements(10);
+        dataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+        return dataSource;
+    }
 }
