1c88648763bfdd6933bf1f290c4b5ec55dc547df
Teijo Rönkä <teijo.ronka@solita.fi>
2018-02-26 12:01:46 +0200

    DPO-220 review fixes

--- src/main/java/fi/livi/digitraffic/meri/controller/exception/SimpleErrorController.java
+++ src/main/java/fi/livi/digitraffic/meri/controller/exception/SimpleErrorController.java
@@ -4,18 +4,19 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
-//@RestController
-//@RequestMapping("/error")
-//@ConditionalOnProperty(value = "spring.main.web_environment", matchIfMissing = true)
+@RestController
+@RequestMapping("/error")
+@ConditionalOnWebApplication
 public class SimpleErrorController implements ErrorController {
-
     private final ErrorAttributes errorAttributes;
 
     private final static boolean includeStackTrace = false;
@@ -32,13 +33,13 @@ public class SimpleErrorController implements ErrorController {
     }
 
     @RequestMapping
-    public Map<String, Object> error(final HttpServletRequest aRequest) {
-        return getErrorAttributes(aRequest);
+    public Map<String, Object> error(final HttpServletRequest request) {
+        return getErrorAttributes(request);
     }
 
-    private Map<String, Object> getErrorAttributes(final HttpServletRequest aRequest) {
-        final RequestAttributes requestAttributes = new ServletRequestAttributes(aRequest);
-        return null;
-        //return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+    private Map<String, Object> getErrorAttributes(final HttpServletRequest request) {
+        final WebRequest webRequest = new ServletWebRequest(request);
+
+        return errorAttributes.getErrorAttributes(webRequest, includeStackTrace);
     }
 }
