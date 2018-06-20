02fb28560d3f5dcf624a4e0d88d9cedca27165b8
Teijo Rönkä <teijo.ronka@solita.fi>
2018-06-20 12:56:25 +0300

    DPO-375 faster listener, faster database update

--- src/main/java/fi/livi/digitraffic/meri/controller/reader/VesselLocationDatabaseListener.java
+++ src/main/java/fi/livi/digitraffic/meri/controller/reader/VesselLocationDatabaseListener.java
@@ -1,8 +1,13 @@
 package fi.livi.digitraffic.meri.controller.reader;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import fi.livi.digitraffic.meri.controller.MessageConverter;
 import fi.livi.digitraffic.meri.dao.ais.VesselLocationRepository;
@@ -16,6 +21,8 @@ public class VesselLocationDatabaseListener implements WebsocketListener {
 
     private final String instanceId;
 
+    private final Map<Integer, AISMessage> messageMap = new HashMap<>();
+
     public VesselLocationDatabaseListener(final VesselLocationRepository vesselLocationRepository,
                                           final LockingService lockingService) {
         this.vesselLocationRepository = vesselLocationRepository;
@@ -24,13 +31,32 @@ public class VesselLocationDatabaseListener implements WebsocketListener {
     }
 
     @Override
-    public void receiveMessage(final String message) {
+    public synchronized void receiveMessage(final String message) {
         final AISMessage ais = MessageConverter.convertLocation(message);
 
-        if(ais.validate() && getLock()) {
-            // repository makes new transaction
-            vesselLocationRepository.save(new VesselLocation(ais));
+        if(ais.validate()) {
+            // remove duplicates, if any
+            messageMap.put(ais.attributes.mmsi, ais);
         }
+    }
+
+    @Scheduled(fixedRate = 1000)
+    private void persistQueue() {
+        final List<AISMessage> messages = removeAllMessages();
+
+        if(getLock()) {
+            final List<VesselLocation> locations = messages.stream().map(VesselLocation::new).collect(Collectors.toList());
+
+            vesselLocationRepository.saveAll(locations);
+        }
+    }
+
+    private synchronized List<AISMessage> removeAllMessages() {
+        final List<AISMessage> messages = new ArrayList(messageMap.keySet());
+
+        messageMap.clear();
+
+        return messages;
     }
 
     private boolean getLock() {
