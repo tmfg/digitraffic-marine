54039b80e729d2912ddd699eab3c0bdcf73d5ddd
Teijo Rönkä <teijo.ronka@solita.fi>
2018-06-29 07:49:09 +0300

    DPO-375 use clock_timestamp() instead of current_timestamp try better locking again

--- src/main/java/fi/livi/digitraffic/meri/util/dao/LockingDao.java
+++ src/main/java/fi/livi/digitraffic/meri/util/dao/LockingDao.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class LockingDao {
@@ -19,13 +20,13 @@ public class LockingDao {
      */
     private static final String MERGE =
         "insert into locking_table(lock_name, instance_id, lock_locked, lock_expires)\n" +
-        "VALUES (:lockName, :instanceId, current_timestamp, current_timestamp + :expirationSeconds::integer * interval '1 second')\n" +
+        "VALUES (:lockName, :instanceId, clock_timestamp(), clock_timestamp() + :expirationSeconds::integer * interval '1 second')\n" +
         "ON CONFLICT (lock_name)\n" +
         "DO UPDATE SET\n" +
         "   instance_id = :instanceId,\n" +
-        "   lock_locked = current_timestamp,\n" +
-        "   lock_expires = current_timestamp + :expirationSeconds::integer * interval '1 second'\n" +
-        "where locking_table.instance_id = :instanceId OR locking_table.lock_expires < current_timestamp";
+        "   lock_locked = clock_timestamp(),\n" +
+        "   lock_expires = clock_timestamp() + :expirationSeconds::integer * interval '1 second'\n" +
+        "where locking_table.instance_id = :instanceId OR locking_table.lock_expires < clock_timestamp()";
 
     private static final String RELEASE =
             "DELETE FROM LOCKING_TABLE LT\n" +
@@ -37,13 +38,14 @@ public class LockingDao {
             "FROM LOCKING_TABLE LT\n" +
             "WHERE LT.LOCK_NAME = :lockName\n" +
             "  AND LT.INSTANCE_ID = :instanceId\n" +
-            "  AND LT.LOCK_EXPIRES > current_timestamp";
+            "  AND LT.LOCK_EXPIRES > clock_timestamp()";
 
     @Autowired
     public LockingDao(final NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Transactional
     public boolean acquireLock(final String lockName, final String callerInstanceId, final int expirationSeconds) {
         final MapSqlParameterSource params = new MapSqlParameterSource("lockName", lockName)
             .addValue("instanceId", callerInstanceId)
@@ -54,6 +56,7 @@ public class LockingDao {
         return hasLock(params);
     }
 
+    @Transactional
     public boolean hasLock(final String lockName, final String callerInstanceId) {
         final MapSqlParameterSource params = new MapSqlParameterSource("lockName", lockName)
             .addValue("instanceId", callerInstanceId);
--- src/main/java/fi/livi/digitraffic/meri/util/service/LockingService.java
+++ src/main/java/fi/livi/digitraffic/meri/util/service/LockingService.java
@@ -20,7 +20,7 @@ import fi.livi.digitraffic.meri.util.dao.LockingDao;
  * This is stateful, it has the information whether the lock for ais was acquired last time or not.  Service tries to acquire lock once a
  * second.
  *
- * This is done to reduce lock-checking from database, because it happends twice for every ais message.
+ * This is done to reduce lock-checking from database, because it happens twice for every ais message.
  *
  */
 @Service
@@ -30,35 +30,30 @@ public class LockingService {
     private final String instanceId;
 
     private final AtomicBoolean hasAisLock = new AtomicBoolean(false);
-    private final AtomicReference<ZonedDateTime> lastAcquirement = new AtomicReference<>();
 
     private static final String AIS_LOCK = "AIS";
 
-    @Autowired
+    private static final Logger log = LoggerFactory.getLogger(LockingService.class);
+
     public LockingService(final LockingDao lockingDao) {
         this.lockingDao = lockingDao;
         this.instanceId = UUID.randomUUID().toString();
 
-        setAisLockValue(false);
+        hasAisLock.set(false);
     }
 
-    public synchronized boolean hasLockForAis() {
-        if(lastAcquirement.get().isBefore(ZonedDateTime.now().minusSeconds(1))) {
-            setAisLockValue(lockingDao.hasLock(AIS_LOCK, instanceId));
-        }
-        return hasAisLock.get();
+    public boolean hasLockForAis() {
+        final boolean fromAtomic = hasAisLock.get();
+        final boolean fromDb = lockingDao.hasLock(AIS_LOCK, instanceId);
+
+        log.info("lock fromAtomic {} fromDb {}", fromAtomic, fromDb);
+
+        return fromDb;
     }
 
-    @Transactional
-    public synchronized boolean acquireLockForAis() {
-        setAisLockValue(acquireLock(AIS_LOCK, instanceId, 2));
-
-        return hasAisLock.get();
-    }
-
-    private void setAisLockValue(final boolean value) {
-        hasAisLock.set(value);
-        lastAcquirement.set(ZonedDateTime.now());
+    @Scheduled(fixedRate = 1000)
+    protected void acquireLockForAis() {
+        hasAisLock.set(acquireLock(AIS_LOCK, instanceId, 2));
     }
 
     private boolean acquireLock(final String lockName, final String callerInstanceId, final int expirationSeconds) {
