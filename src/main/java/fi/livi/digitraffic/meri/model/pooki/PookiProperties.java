caefee11722835bc2479d027aff2c9ee66ad8b67
Ville Kähkönen <ville.kahkonen@solita.fi>
2018-10-30 16:46:00 +0200

    DPO-637 fix PookiProperties Jackson serialization / deserialization

--- src/main/java/fi/livi/digitraffic/meri/model/pooki/PookiProperties.java
+++ src/main/java/fi/livi/digitraffic/meri/model/pooki/PookiProperties.java
@@ -6,6 +6,7 @@ import java.time.ZonedDateTime;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
@@ -19,142 +20,166 @@ import io.swagger.annotations.ApiModelProperty;
 public class PookiProperties implements Serializable {
 
     @ApiModelProperty(required = true)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer id;
 
     // Merialueet suomeksi
     @ApiModelProperty(name="areastest", value = "Area in Finnish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String areasFi;
 
     @ApiModelProperty("Area in Swedish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String areasSv;
 
     @ApiModelProperty("Area in English")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String areasEn;
 
     // Numerointi alkaa jokaisen vuoden alussa aina ykkösestä ja annetaan vasta sitten kun varoituksen tila muuttuu julkaistuksi
     @ApiModelProperty("Nautical warning number. Starts with the digit 1 at the begining of the year. Set when warning was published.")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer number;
 
     // Sijainnin tarkennus suomeksi
     @ApiModelProperty("Location specifier in Finnish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String locationFi;
 
     @ApiModelProperty("Location specifier in Swedish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String locationSv;
 
     @ApiModelProperty("Location specifier in English")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String locationEn;
 
     // Merivaroituksen sisältö suomeksi
     @ApiModelProperty("Nautical warning contents in Finnish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String contentsFi;
 
     @ApiModelProperty("Nautical warning contents in Swedish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String contentsSv;
 
     @ApiModelProperty("Nautical warning contents in English")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String contentsEn;
 
     // Päiväys ja aika
     @ApiModelProperty("Set when nautical warning was created")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private ZonedDateTime creationTime;
 
     // Merivaroitustyypin selite suomeksi
     @ApiModelProperty("Nautical warning type in Finnish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String typeFi;
 
     @ApiModelProperty("Nautical warning type in Swedish")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String typeSv;
 
     @ApiModelProperty("Nautical warning type in English")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String typeEn;
 
     @ApiModelProperty("Beginning of validity time")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private ZonedDateTime validityStartTime;
 
     @ApiModelProperty("End of validity time")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private ZonedDateTime validityEndTime;
 
     // Should this be hidden?
     @ApiModelProperty("Nautical warning contents for tooltip")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String tooltip;
 
     @ApiModelProperty("Are there virtual navaids related to this warning")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Boolean virtualNavaids;
 
     @ApiModelProperty("Is navtex message")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Boolean navtex;
 
     // Merivaroitukseen liittyvän turvalaitteen ominaisuustiedot  (rivinvaihdoilla eroteltuina)
     @ApiModelProperty("Aids to navigation related to the nautical warning")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String navaidInfo;
 
     // Merivaroitukseen liittyvän väyläalueen ominaisuustiedot (rivinvaihdoilla eroteltuina)
     @ApiModelProperty("Fairway features related to the nautical warning (separated by line breaks)")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String fairwayInfo;
 
     // Merivaroitukseen liittyvän navigointilinjan ominaisuustiedot (rivinvaihdoilla eroteltuina)
     @ApiModelProperty("Navigation line features related to the nautical warning (separated by line breaks)")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String navigationLineInfo;
 
     @ApiModelProperty("Nautical warning publishing time")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private ZonedDateTime publishingTime;
 
     @ApiModelProperty("Notificator")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String notificator;
 
-    public PookiProperties(final Integer id,
-                           final String alueetFi,
-                           final String alueetSv,
-                           final String alueetEn,
-                           final Integer number,
-                           final String sijaintiFi,
-                           final String sijaintiSv,
-                           final String sijaintiEn,
-                           final String sisaltoFi,
-                           final String sisaltoSv,
-                           final String sisaltoEn,
-                           @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime paivays,
-                           final String tyyppiFi,
-                           final String tyyppiSv,
-                           final String tyyppiEn,
-                           @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime voimassaAlkaa,
-                           @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime voimassaPaattyy,
-                           final String valittykohdeTooltip,
-                           final Boolean virtuaalinenturvalaite,
-                           final Boolean navtex,
-                           @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class)                         final ZonedDateTime asdfTallennuspaiva,
-                           final String turvalaiteTxt,
-                           final String vaylaalueTxt,
-                           final String navigointilinjaTxt,
-                           @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final
-                           ZonedDateTime antopaiva,
-                           final String tiedoksiantaja) {
+    public PookiProperties(@JsonProperty(value = "ID", access = JsonProperty.Access.WRITE_ONLY) final Integer id,
+                           @JsonProperty(value = "ALUEET_FI", access = JsonProperty.Access.WRITE_ONLY) final String areasFi,
+                           @JsonProperty(value = "ALUEET_SV", access = JsonProperty.Access.WRITE_ONLY) final String areasSv,
+                           @JsonProperty(value = "ALUEET_EN", access = JsonProperty.Access.WRITE_ONLY) final String areasEn,
+                           @JsonProperty(value = "NUMERO", access = JsonProperty.Access.WRITE_ONLY) final Integer number,
+                           @JsonProperty(value = "SIJAINTI_FI", access = JsonProperty.Access.WRITE_ONLY) final String locationFi,
+                           @JsonProperty(value = "SIJAINTI_SV", access = JsonProperty.Access.WRITE_ONLY) final String locationSv,
+                           @JsonProperty(value = "SIJAINTI_EN", access = JsonProperty.Access.WRITE_ONLY) final String locationEn,
+                           @JsonProperty(value = "SISALTO_FI", access = JsonProperty.Access.WRITE_ONLY) final String contentsFi,
+                           @JsonProperty(value = "SISALTO_SV", access = JsonProperty.Access.WRITE_ONLY) final String contentsSv,
+                           @JsonProperty(value = "SISALTO_EN", access = JsonProperty.Access.WRITE_ONLY) final String contentsEn,
+                           @JsonProperty(value = "PAIVAYS", access = JsonProperty.Access.WRITE_ONLY) @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime creationTime,
+                           @JsonProperty(value = "TYYPPI_FI", access = JsonProperty.Access.WRITE_ONLY) final String typeFi,
+                           @JsonProperty(value = "TYYPPI_SV", access = JsonProperty.Access.WRITE_ONLY) final String typeSv,
+                           @JsonProperty(value = "TYYPPI_EN", access = JsonProperty.Access.WRITE_ONLY) final String typeEn,
+                           @JsonProperty(value = "VOIMASSA_ALKAA", access = JsonProperty.Access.WRITE_ONLY) @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime validityStartTime,
+                           @JsonProperty(value = "VOIMASSA_PAATTYY", access = JsonProperty.Access.WRITE_ONLY) @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime validityEndTime,
+                           @JsonProperty(value = "VALITTUKOHDE_TOOLTIP", access = JsonProperty.Access.WRITE_ONLY) final String tooltip,
+                           @JsonProperty(value = "VIRTUAALINENTURVALAITE", access = JsonProperty.Access.WRITE_ONLY) final Boolean virtualNavaids,
+                           @JsonProperty(value = "NAVTEX", access = JsonProperty.Access.WRITE_ONLY) final Boolean navtex,
+                           @JsonProperty(value = "asdfTALLENNUSPAIVA", access = JsonProperty.Access.WRITE_ONLY) @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime recordingTime,
+                           @JsonProperty(value = "TURVALAITE_TXT", access = JsonProperty.Access.WRITE_ONLY) final String navaidInfo,
+                           @JsonProperty(value = "VAYLAALUE_TXT", access = JsonProperty.Access.WRITE_ONLY) final String fairwayInfo,
+                           @JsonProperty(value = "NAVIGOINTILINJA_TXT", access = JsonProperty.Access.WRITE_ONLY) final String navigationLineInfo,
+                           @JsonProperty(value = "ANTOPAIVA", access = JsonProperty.Access.WRITE_ONLY) @JsonDeserialize(using = JsonDateTimeDeserializerToZonedDateTime.class) final ZonedDateTime publishingTime,
+                           @JsonProperty(value = "TIEDOKSIANTAJA", access = JsonProperty.Access.WRITE_ONLY) final String notificator) {
         this.id = id;
-        this.areasFi = alueetFi;
-        this.areasSv = alueetSv;
-        this.areasEn = alueetEn;
+        this.areasFi = areasFi;
+        this.areasSv = areasSv;
+        this.areasEn = areasEn;
         this.number = number;
-        this.locationFi = sijaintiFi;
-        this.locationSv = sijaintiSv;
-        this.locationEn = sijaintiEn;
-        this.contentsFi = sisaltoFi;
-        this.contentsSv = sisaltoSv;
-        this.contentsEn = sisaltoEn;
-        this.creationTime = paivays;
-        this.typeFi = tyyppiFi;
-        this.typeSv = tyyppiSv;
-        this.typeEn = tyyppiEn;
-        this.validityStartTime = voimassaAlkaa;
-        this.validityEndTime = voimassaPaattyy;
-        this.tooltip = valittykohdeTooltip;
-        this.virtualNavaids = virtuaalinenturvalaite;
+        this.locationFi = locationFi;
+        this.locationSv = locationSv;
+        this.locationEn = locationEn;
+        this.contentsFi = contentsFi;
+        this.contentsSv = contentsSv;
+        this.contentsEn = contentsEn;
+        this.creationTime = creationTime;
+        this.typeFi = typeFi;
+        this.typeSv = typeSv;
+        this.typeEn = typeEn;
+        this.validityStartTime = validityStartTime;
+        this.validityEndTime = validityEndTime;
+        this.tooltip = tooltip;
+        this.virtualNavaids = virtualNavaids;
         this.navtex = navtex;
-        this.navaidInfo = turvalaiteTxt;
-        this.fairwayInfo = vaylaalueTxt;
-        this.navigationLineInfo = navigointilinjaTxt;
-        this.publishingTime = antopaiva;
-        this.notificator = tiedoksiantaja;
+        this.navaidInfo = navaidInfo;
+        this.fairwayInfo = fairwayInfo;
+        this.navigationLineInfo = navigationLineInfo;
+        this.publishingTime = publishingTime;
+        this.notificator = notificator;
     }
 
     @Override
