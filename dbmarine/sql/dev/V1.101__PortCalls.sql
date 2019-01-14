cd9a1ced285927dd48ecfe7a604b37161711880e
Teijo Rönkä <teijo.ronka@solita.fi>
2019-01-14 09:49:58 +0200

    insert some test port_calls

---
+++ dbmarine/sql/dev/V1.101__PortCalls.sql
@@ -0,0 +1,5 @@
+insert into port_call(port_call_id,port_call_timestamp, port_to_visit, prev_port, next_port, vessel_name, vessel_name_prefix, radio_call_sign, radio_call_sign_type)
+values (1, '2019-01-01 10:00:00', 'FIHEL', 'FITKU', 'FIKOK', 'test1', 'ms', 't1', 't1');
+
+insert into port_call(port_call_id,port_call_timestamp, port_to_visit, prev_port, next_port, vessel_name, vessel_name_prefix, radio_call_sign, radio_call_sign_type)
+values (2, '2019-01-01 12:00:00', 'FITKU', 'FIRAU', 'FIPOR', 'test2', 'hs', 't2', 't2');
