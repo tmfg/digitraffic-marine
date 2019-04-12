2940ba350c6ca7f9070b82af6cf60691ddc4682f
Jouni <jouni.sonninen@solita.fi>
2019-04-12 13:25:49 +0200

    DPO-442 Merenkulun älypoijudata - Swagger doc updates

--- src/main/java/fi/livi/digitraffic/meri/model/RootDataObjectDto.java
+++ src/main/java/fi/livi/digitraffic/meri/model/RootDataObjectDto.java
@@ -3,9 +3,14 @@ package fi.livi.digitraffic.meri.model;
 import java.time.ZonedDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import fi.livi.digitraffic.meri.model.geojson.FeatureCollection;
 import io.swagger.annotations.ApiModelProperty;
 
+@JsonPropertyOrder({ "type",
+                     "dataUpdatedTime",
+                     "features" })
 public class RootDataObjectDto<FeatureType> extends FeatureCollection<FeatureType> {
 
     @ApiModelProperty(value = "Data last updated timestamp in ISO 8601 format with time offsets from UTC (eg. 2016-04-20T12:38:16.328+03:00 or 2018-11-09T09:41:09Z)", required = true)
--- src/main/java/fi/livi/digitraffic/meri/model/sse/SseFeatureCollection.java
+++ src/main/java/fi/livi/digitraffic/meri/model/sse/SseFeatureCollection.java
@@ -3,16 +3,9 @@ package fi.livi.digitraffic.meri.model.sse;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
 import fi.livi.digitraffic.meri.model.RootDataObjectDto;
 import io.swagger.annotations.ApiModel;
 
-@JsonPropertyOrder({
-        "type",
-        "dateUpdatedTime",
-        "features",
-})
 @ApiModel(description = "GeoJSON FeatureCollection object of SSE data")
 public class SseFeatureCollection extends RootDataObjectDto<SseFeature> {
 
