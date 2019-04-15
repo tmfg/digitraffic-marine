93749c360d469275c130d0d10ad4380bf345f66f
Jouni <jouni.sonninen@solita.fi>
2019-04-15 11:53:25 +0200

    DPO-442 Merenkulun älypoijudata - Fixed test

--- src/main/java/fi/livi/digitraffic/meri/service/sse/SseService.java
+++ src/main/java/fi/livi/digitraffic/meri/service/sse/SseService.java
@@ -60,7 +60,7 @@ public class SseService {
             count++;
             log.info("method=saveTlscSseReports report=\n{}", StringUtil.toJsonString(saved));
         }
-        updatedTimestampRepository.setUpdated(SSE_DATA, ZonedDateTime.now(), getClass().getSimpleName());
+        updatedTimestampRepository.setUpdated(SSE_DATA, ZonedDateTime.now(), SseService.class.getSimpleName());
         log.info("method=saveTlscSseReports countSaved={}", count);
         return count;
     }
