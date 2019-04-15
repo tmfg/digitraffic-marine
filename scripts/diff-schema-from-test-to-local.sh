5e085543a4ef5941dfb81d820cabde50480cdafa
Jouni <jouni.sonninen@solita.fi>
2019-04-15 11:42:39 +0200

    DPO-442 Merenkulun älypoijudata - scripts and test data

---
+++ scripts/diff-schema-from-test-to-local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+curl -s https://meri-test.digitraffic.fi/api/v1/metadata/documentation/v2/api-docs?group=metadata-api | jq > test.json && curl -s http://localhost:9001/v2/api-docs?group=metadata-api |jq > local.json && diffmerge test.json local.json
