23b08006c5c071a924c1c742fb602f90932e7bd1
Jouni <jouni.sonninen@solita.fi>
2019-05-14 11:39:30 +0200

    DPO-442 Merenkulun älypoijudata - Fixed 406 Media type not acceptable when requesting with accept header "application/geo+json"

--- src/main/java/fi/livi/digitraffic/meri/controller/BetaController.java
+++ src/main/java/fi/livi/digitraffic/meri/controller/BetaController.java
@@ -35,6 +35,7 @@ public class BetaController {
     private static final Logger log = LoggerFactory.getLogger(BetaController.class);
 
     public static final String MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE = "application/vnd.geo+json";
+    public static final String MEDIA_TYPE_APPLICATION_GEO_JSON_VALUE = "application/geo+json";
 
     public static final String CODE_400_SEARCH_RESULT_TOO_BIG = "The search result is too big (over 1000 items)";
     public static final String CODE_400_NOT_EXISTS_WITH_IDENTIFIER = "Objects not exists with given identifier";
@@ -50,7 +51,9 @@ public class BetaController {
     }
 
     @ApiOperation("Return latest SSE (Sea State Estimation) data as GeoJSON")
-    @GetMapping(path = API_SSE_PATH + LATEST_PATH , produces = { MediaType.APPLICATION_JSON_UTF8_VALUE, MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE })
+    @GetMapping(path = API_SSE_PATH + LATEST_PATH , produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
+                                                                 MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE,
+                                                                 MEDIA_TYPE_APPLICATION_GEO_JSON_VALUE })
     @ResponseBody
     public SseFeatureCollection findLatest() {
         return sseService.findLatest();
@@ -61,7 +64,8 @@ public class BetaController {
         @ApiResponse(code = 400, message = CODE_400_NOT_EXISTS_WITH_IDENTIFIER)
     })
     @GetMapping(path = API_SSE_PATH + LATEST_PATH + "/{siteNumber}", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
-                                                                                  MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE })
+                                                                                  MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE,
+                                                                                  MEDIA_TYPE_APPLICATION_GEO_JSON_VALUE } )
     @ResponseBody
     public SseFeatureCollection findLatest(
         @ApiParam(value = "SSE site number", required = true)
@@ -76,7 +80,9 @@ public class BetaController {
         @ApiResponse(code = 400, message = CODE_400_SEARCH_RESULT_TOO_BIG + " or " +
                                            CODE_400_ILLEGAL_ARGUMENTS)
     })
-    @GetMapping(path = API_SSE_PATH + HISTORY_PATH, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE, MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE })
+    @GetMapping(path = API_SSE_PATH + HISTORY_PATH, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
+                                                                 MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE,
+                                                                 MEDIA_TYPE_APPLICATION_GEO_JSON_VALUE })
     @ResponseBody
     public SseFeatureCollection findHistory(
 
@@ -100,7 +106,8 @@ public class BetaController {
                                            CODE_400_ILLEGAL_ARGUMENTS)
     })
     @GetMapping(path = API_SSE_PATH + HISTORY_PATH + "/{siteNumber}", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE,
-                                                                                   MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE })
+                                                                                   MEDIA_TYPE_APPLICATION_VND_GEO_JSON_VALUE,
+                                                                                   MEDIA_TYPE_APPLICATION_GEO_JSON_VALUE })
     @ResponseBody
     public SseFeatureCollection findHistoryBySite(
         @ApiParam(value = "SSE site number", required = true)
