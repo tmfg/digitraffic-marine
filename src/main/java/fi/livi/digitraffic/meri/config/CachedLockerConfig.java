1f6999f7ccc84da5fcbebcac95301787e64bcf6a
Jouni <jouni.sonninen@solita.fi>
2019-05-16 12:46:40 +0200

    DPO-442 Merenkulun älypoijudata - Review fix

--- src/main/java/fi/livi/digitraffic/meri/config/CachedLockerConfig.java
+++ src/main/java/fi/livi/digitraffic/meri/config/CachedLockerConfig.java
@@ -1,6 +1,5 @@
 package fi.livi.digitraffic.meri.config;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +11,6 @@ import fi.livi.digitraffic.meri.util.service.LockingService;
 @ConditionalOnExpression("'${config.test}' != 'true'")
 @Configuration
 public class CachedLockerConfig {
-    private final String clientId = "marine_updater_" + MqttClient.generateClientId();
 
     @Bean
     @ConditionalOnExpression("'${ais.mqtt.enabled}' == 'true' or '${ais.reader.enabled}' == 'true'")
