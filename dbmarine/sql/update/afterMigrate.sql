1fc4b1bbd0ab6e2f880651724884816731890182
Teijo Rönkä <teijo.ronka@solita.fi>
2019-06-26 08:40:15 +0300

    DPO-813 reindex qrtz-tables

--- dbmarine/sql/update/afterMigrate.sql
+++ dbmarine/sql/update/afterMigrate.sql
@@ -1,4 +1,4 @@
--- reindex with every db update
+-- reindex after every db update
 reindex table vessel;
 reindex table vessel_location;
 reindex table agent_info;
@@ -7,3 +7,9 @@ reindex table port_area_details;
 reindex table sse_report;
 reindex table ssn_location;
 reindex table updated_timestamp;
+reindex table ship_activity;
+
+-- reindex qrtz_tables
+reindex table qrtz_fired_triggers;
+reindex table qrtz_simple_triggers;
+reindex table qrtz_triggers;
