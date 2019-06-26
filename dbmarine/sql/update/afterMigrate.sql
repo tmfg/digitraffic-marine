a5039b0bb6f007039ec0a0599939d42027f1d72b
Teijo Rönkä <teijo.ronka@solita.fi>
2019-06-26 08:32:43 +0300

    DPO-813 reindex

---
+++ dbmarine/sql/update/afterMigrate.sql
@@ -0,0 +1,9 @@
+-- reindex with every db update
+reindex table vessel;
+reindex table vessel_location;
+reindex table agent_info;
+reindex table cargo_info;
+reindex table port_area_details;
+reindex table sse_report;
+reindex table ssn_location;
+reindex table updated_timestamp;
