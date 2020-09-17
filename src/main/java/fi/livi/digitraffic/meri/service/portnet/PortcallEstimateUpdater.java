1d781c091c83ec843370d4be54e5378697a99418
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-09-17 16:24:48 +0300

    DPO-1187 remove logging

--- src/main/java/fi/livi/digitraffic/meri/service/portnet/PortcallEstimateUpdater.java
+++ src/main/java/fi/livi/digitraffic/meri/service/portnet/PortcallEstimateUpdater.java
@@ -122,7 +122,6 @@ class HttpPortcallEstimateUpdater implements PortcallEstimateUpdater {
                 final HttpPost post = new HttpPost(portcallEstimateUrl);
                 post.setHeader("X-Api-Key", portcallEstimateApiKey);
                 final String json = om.writeValueAsString(pce);
-                log.info("method=updatePortcallEstimate about to send json {} to {}", json, portcallEstimateUrl);
                 post.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
                 final StatusLine status = httpClient.execute(post).getStatusLine();
                 if (status.getStatusCode() != HttpStatus.SC_OK) {
