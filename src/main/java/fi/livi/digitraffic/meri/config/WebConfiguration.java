286671cced2c21949382aea9403e06bde7d87186
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-12-30 07:07:03 +0200

    DPO-1194 add etag filter

--- src/main/java/fi/livi/digitraffic/meri/config/WebConfiguration.java
+++ src/main/java/fi/livi/digitraffic/meri/config/WebConfiguration.java
@@ -1,12 +1,21 @@
 package fi.livi.digitraffic.meri.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import javax.servlet.Filter;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public Filter ShallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
     @Override
     public void configurePathMatch(final PathMatchConfigurer matcher) {
         // Allow dots in {from} part in request: GET /locations/mmsi/{mmsi}/radius/{radius}/from/{from}
