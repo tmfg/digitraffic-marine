b801587bb6a027f81848255de3a785e6d1f3b580
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-01-26 16:23:15 +0200

    DPO-1194 Weak Etags don't work either, ditch it

--- src/main/java/fi/livi/digitraffic/meri/config/WebConfiguration.java
+++ src/main/java/fi/livi/digitraffic/meri/config/WebConfiguration.java
@@ -1,22 +1,13 @@
 package fi.livi.digitraffic.meri.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import javax.servlet.Filter;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-
-    @Bean
-    public Filter ShallowEtagHeaderFilter() {
-        final ShallowEtagHeaderFilter shallowEtagHeaderFilter = new ShallowEtagHeaderFilter();
-        shallowEtagHeaderFilter.setWriteWeakETag(true);
-        return shallowEtagHeaderFilter;
-    }
 
     @Override
     public void configurePathMatch(final PathMatchConfigurer matcher) {
