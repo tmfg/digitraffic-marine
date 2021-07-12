ec552b3df0383db8b3119c3d3f67f8adb2425f49
Jouni Sonninen <jouni.sonninen@solita.fi>
2021-07-12 13:57:55 +0300

    DPO-785 Scheduled-annotaation virhekäsittely

---
+++ src/main/java/fi/livi/digitraffic/meri/config/ScheduledAnnotationConfiguration.java
@@ -0,0 +1,26 @@
+package fi.livi.digitraffic.meri.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Purpose of this class is just to have possibility for tests to
+ * deactivated the @scheduled jobs in the tests.
+ */
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(
+    name = "dt.scheduled.annotation.enabled",
+    havingValue = "true",
+    matchIfMissing = true)
+public class ScheduledAnnotationConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledAnnotationConfiguration.class);
+
+    public ScheduledAnnotationConfiguration() {
+        // empty, just to init @EnableScheduling annotation
+        log.info("Scheduled annotation enabled");
+    }
+}
