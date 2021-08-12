a4fba6575741c14e8982a6479bad6c8f4cf15642
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-08-12 10:20:04 +0300

    DPO-1476 Add columns for brighten message sent and assumed brighten time

--- dbmarine/sql/update/V2/V2.6.9__DPO-1476_areatraffic.sql
+++ dbmarine/sql/update/V2/V2.6.9__DPO-1476_areatraffic.sql
@@ -1,6 +1,8 @@
 CREATE TABLE areatraffic(
   id BIGINT PRIMARY KEY NOT NULL,
   name TEXT NOT NULL,
-  lighting_duration_min SMALLINT NOT NULL,
-  geometry GEOMETRY NOT NULL
+  brighten_duration_min SMALLINT NOT NULL,
+  geometry GEOMETRY NOT NULL,
+  brighten_sent TIMESTAMP WITH TIME ZONE,
+  brighten_end TIMESTAMP WITH TIME ZONE
 );
