4f8669126374fa6f0deec437f6178e69e5b50035
Jouni Sonninen <jouni.sonninen@solita.fi>
2021-09-07 12:43:40 +0200

    DPO-1466 SSE-datan vastaanoton koodin poisto sovelluksesta - fixed import

--- src/main/java/fi/livi/digitraffic/meri/service/sse/SseService.java
+++ src/main/java/fi/livi/digitraffic/meri/service/sse/SseService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
