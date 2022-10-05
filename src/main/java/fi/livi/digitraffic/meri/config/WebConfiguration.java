eac3b7a8c696183ae558033543e1ee65c084d98e
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-10-05 16:35:20 +0300

    DPO-1956 Add Deprecation and Sunset headers

--- src/main/java/fi/livi/digitraffic/meri/config/WebConfiguration.java
+++ src/main/java/fi/livi/digitraffic/meri/config/WebConfiguration.java
@@ -1,13 +1,14 @@
 package fi.livi.digitraffic.meri.config;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.Filter;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -27,5 +28,10 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/swagger-ui/index.html");
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new DeprecationInterceptor());
     }
 }
