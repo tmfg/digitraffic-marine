f71091a00059d137672c3690049583b3a7498efb
Jouni Sonninen <jouni.sonninen@solita.fi>
2023-04-28 14:19:26 +0200

    Update app version resolving service and added tags to actuator info

--- src/main/java/fi/livi/digitraffic/meri/service/BuildVersionService.java
+++ src/main/java/fi/livi/digitraffic/meri/service/BuildVersionService.java
@@ -1,32 +1,75 @@
 package fi.livi.digitraffic.meri.service;
 
-import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.Properties;
 
-import com.jcabi.manifests.Manifests;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import fi.livi.digitraffic.meri.annotation.NotTransactionalServiceMethod;
 
 @Service
 public class BuildVersionService {
 
-    @NotTransactionalServiceMethod
-    public String getAppVersion() {
-        if (Manifests.exists("MarineApplication-Version")) {
-            return Manifests.read("MarineApplication-Version");
+    private static final Logger log = LoggerFactory.getLogger(BuildVersionService.class);
+
+    private static final String GIT_PROPERTIES  = "git.properties";
+    private static final String GIT_COMMIT_ID_HASH = "git.commit.id.abbrev";
+    private static final String GIT_BUILD_TIME  = "git.build.time";
+    private static final String GIT_TAGS  = "git.tags";
+    private static final String GIT_DIRTY  = "git.dirty";
+
+    private String getAppBuildRevision() {
+        final String tag = readProperty(GIT_COMMIT_ID_HASH);
+        if (StringUtils.isNotBlank(tag)) {
+            return tag;
         }
-        return "DEV-BUILD";
+        return "?";
     }
 
-    @NotTransactionalServiceMethod
-    public String getAppBuildRevision() {
-        if (Manifests.exists("MarineApplication-Build")) {
-            return Manifests.read("MarineApplication-Build");
+    private String getAppBuildTime() {
+        final String time = readProperty(GIT_BUILD_TIME);
+        if (StringUtils.isNotBlank(time)) {
+            return time;
         }
-        return "X";
+        return "?";
+    }
+
+    private String getAppTags() {
+        final String tags = readProperty(GIT_TAGS);
+        if (StringUtils.isNotBlank(tags) && !StringUtils.contains(tags, "{")) {
+            return tags;
+        }
+        return "DEV";
+    }
+
+    private String getDirty() {
+        final String dirty = readProperty(GIT_DIRTY);
+        if (StringUtils.isNotBlank(dirty) && !StringUtils.contains(dirty, "{")) {
+            return "true".equals(dirty) ? "dirty" : "";
+        }
+        return "dirty";
     }
 
     @NotTransactionalServiceMethod
     public String getAppFullVersion() {
-        return getAppVersion() + "-" + getAppBuildRevision();
+        return String.format("Tag: %s #%s %s @ %s", getAppTags(), getAppBuildRevision(), getDirty(), getAppBuildTime());
+    }
+
+    private String readProperty(final String property) {
+        final Properties properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream(GIT_PROPERTIES));
+            final String prop = (String) properties.get(property);
+            if (StringUtils.isNotBlank(prop)) {
+                return prop;
+            }
+            return null;
+        } catch (IOException ioe) {
+            log.error("Failed to load git properties from file: " + GIT_PROPERTIES, ioe);
+            return null;
+        }
     }
 }
