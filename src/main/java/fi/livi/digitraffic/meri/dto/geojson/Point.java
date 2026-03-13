56f0691d4bf4803fb7af817d5143e86f984f1868
Joonas-M-S <joonas.mikkonen@solita.fi>
2026-03-13 15:17:25 +0200

    Add JsonCreator to Point

--- src/main/java/fi/livi/digitraffic/meri/dto/geojson/Point.java
+++ src/main/java/fi/livi/digitraffic/meri/dto/geojson/Point.java
@@ -3,11 +3,14 @@ package fi.livi.digitraffic.meri.dto.geojson;
 import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "GeoJSON Point Geometry object")
 public class Point extends Geometry<List<Double>> {
 
+    @JsonCreator
     public Point() {
         super(GeometryType.Point);
     }
